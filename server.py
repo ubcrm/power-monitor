2a10095d0366d56b50d454d1901e2140d088affd
ShreyansK2000 <gshreyansk4@gmail.com>
2020-02-29 16:40:23 -0800

    phidget code and threading

--- server.py
+++ server.py
@@ -1,47 +1,89 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-import _thread
+#import _thread
+import threading
 from random import randint
 from random import seed
+
+import atexit
+
+from Phidget22.Phidget import *
+from Phidget22.Devices.VoltageInput import *
+from Phidget22.Devices.CurrentInput import *
 
 app = Flask(__name__)
 CORS(app)
 
-currentVal = None
-voltageVal = None
-powerVal = None
+current = None
+voltage = None
+power = None
 seed(1)
+
+currentThread = None
+voltageThread = None
+powerThread = None
+
+voltageCh = None
+currentCh = None
+
+threads = [currentThread, voltageThread, powerThread]
 
 @app.route('/getVals', methods=['GET'])
 def getIandV():
-    global currentVal, voltageVal, powerVal
-    while (powerVal == None):
-        while (voltageVal == None):
-            while (currentVal == None):
+    global current, voltage, power
+    while (power == None):
+        while (voltage == None):
+            while (current == None):
                 pass
-
-    while (powerVal is not currentVal * voltageVal):
-        pass
     
-    return jsonify({"current": currentVal,"voltage": voltageVal, "power": powerVal})
+    return jsonify({"current": current,"voltage": voltage, "power": power})
 
-
-def getPhidgetValues():
-    global currentVal, voltageVal, powerVal
-
-    # change these values to get from phidget code
-    currentVal = randint(0, 10)
-    voltageVal = randint(0, 10)
-    powerVal = currentVal * voltageVal
-
-def doThreading():
+def getCurrentVal(ch):
+    global current
     while True:
-        getPhidgetValues()
+        current = ch.getCurrent()
+        
+def getVoltageVal(ch):
+    global voltage
+    while True:
+        voltage = ch.getVoltage()
+        
+def calculatePower():
+    global current, voltage, power
+    while True:
+        power = current * voltage
+        
+        
+def exitHandler():
+    global threads, voltageCh, currentCh
+    
+    if voltageCh is not None:
+        voltageCh.close()
+    
+    if currentCh is not None:
+        currentCh.close()
+    
+    for thread in threads:
+        if thread is not None:
+            thread.join()
 
 if __name__ == "__main__":
-    _thread.start_new_thread(doThreading, ())
-    print("thread started")
+    #_thread.start_new_thread(doThreading, ())
+    voltageCh = VoltageInput()
+    voltageCh.openWaitForAttachment(1000)
+    
+    currentCh = CurrentInput()
+    currentCh.openWaitForAttachment(1000)
+    
+    currentThread = threading.Thread(target = getCurrentVal, args = (currentCh))
+    voltageThread = threading.Thread(target = getVoltageVal, args = (voltageCh))
+    powerThread   = threading.Thread(target = calculatePower, args = ())
+    for thread in threads:
+        if thread is not None:
+            thread.start()
+            
+    atexit.register(exitHandler)
     app.run(threaded=True, debug=True, host='0.0.0.0')
 
 
