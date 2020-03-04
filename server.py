f9a88636de87ba7064a6f33dc4563e01fcc39122
ShreyansK2000 <gshreyansk4@gmail.com>
2020-03-03 20:19:12 -0800

    new phidget changes and index file

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
 
 #import _thread
@@ -10,7 +10,9 @@ import atexit
 
 from Phidget22.Phidget import *
 from Phidget22.Devices.VoltageInput import *
-from Phidget22.Devices.CurrentInput import *
+from Phidget22.Devices.VoltageRatioInput import *
+from Phidget22.Devices.DigitalOutput import *
+from Phidget22.PhidgetException import *
 
 app = Flask(__name__)
 CORS(app)
@@ -20,14 +22,20 @@ voltage = None
 power = None
 seed(1)
 
+'''
 currentThread = None
 voltageThread = None
 powerThread = None
 
+threads = [currentThread, voltageThread, powerThread]
+'''
+
 voltageCh = None
 currentCh = None
 
-threads = [currentThread, voltageThread, powerThread]
+@app.route('/', methods=['GET'])
+def getIndex():
+    return render_template("index.html")
 
 @app.route('/getVals', methods=['GET'])
 def getIandV():
@@ -39,6 +47,7 @@ def getIandV():
     
     return jsonify({"current": current,"voltage": voltage, "power": power})
 
+'''
 def getCurrentVal(ch):
     global current
     while True:
@@ -48,6 +57,16 @@ def getVoltageVal(ch):
     global voltage
     while True:
         voltage = ch.getVoltage()
+   
+'''
+
+def currentChangeHandler(self, current_update):
+    global current
+    current = current_update * 75.85973 - 37.76251
+
+def voltageChangeHandler(self, voltage_update):
+    global voltage
+    voltage = voltage_update
         
 def calculatePower():
     global current, voltage, power
@@ -56,7 +75,8 @@ def calculatePower():
         
         
 def exitHandler():
-    global threads, voltageCh, currentCh
+    global voltageCh, currentCh
+    global powerThread
     
     if voltageCh is not None:
         voltageCh.close()
@@ -64,25 +84,53 @@ def exitHandler():
     if currentCh is not None:
         currentCh.close()
     
+    if powerThread is not None:
+        powerThread.join()
+    '''
     for thread in threads:
         if thread is not None:
             thread.join()
 
+    '''
+    
 if __name__ == "__main__":
     #_thread.start_new_thread(doThreading, ())
-    voltageCh = VoltageInput()
-    voltageCh.openWaitForAttachment(1000)
     
-    currentCh = CurrentInput()
-    currentCh.openWaitForAttachment(1000)
+    try:
+        # voltage channel initialization
+        voltageCh = VoltageInput()
+        voltageCh.setHubPort(0)
+        voltageCh.setIsHubPortDevice(False)
+        voltageCh.setOnVoltageChangeHandler(voltageChangeHandler)
+        
+        
+        # current channel initialization
+        currentCh = VoltageRatioInput()
+        currentCh.setHubPort(1)
+        currentCh.setIsHubPortDevice(True)
+        currentCh.setOnVoltageRatioChangeHandler(currentChangeHandler)
+        
+        currentCh.openWaitForAttachment(1000)
+
+    except PhidgetException as PE:
+        print("Phidget Exception:\n{0}".format(PE))
+        
+    while True:
+        power = current * voltage
     
+    '''
     currentThread = threading.Thread(target = getCurrentVal, args = (currentCh))
     voltageThread = threading.Thread(target = getVoltageVal, args = (voltageCh))
-    powerThread   = threading.Thread(target = calculatePower, args = ())
+    
     for thread in threads:
         if thread is not None:
             thread.start()
-            
+    '''
+    
+    powerThread   = threading.Thread(target = calculatePower, args = ())
+    if powerThread is not None:
+        powerThread.start()
+        
     atexit.register(exitHandler)
     app.run(threaded=True, debug=True, host='0.0.0.0')
 
